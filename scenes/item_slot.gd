68397e7cd954412bc7839f21394f5ac18865b17f
mnoun09 <monikanoun2009@gmail.com>
2026-01-25 15:11:59 -0800

    Implementing drag and drop

--- scenes/item_slot.gd
+++ scenes/item_slot.gd
@@ -1,6 +1,9 @@
 extends Panel
 
 @onready var icon: TextureRect = $Icon
+var draggable = false
+var is_inside_dropable = false
+var body_ref
 
 func _get_drag_data(at_position: Vector2) -> Variant:
 	if icon.texture == null:
@@ -10,14 +13,22 @@ func _get_drag_data(at_position: Vector2) -> Variant:
 	var c = Control.new()
 	c.add_child(preview)
 	preview.position -= Vector2(50,50)
-	preview.scale = Vector2(0.5, 0.5)
+	preview.scale = Vector2(0.8, 0.8)
 	preview.self_modulate = Color.TRANSPARENT
 	c.modulate = Color(c.modulate, 0.5)
 	set_drag_preview(c)
 	return icon
 
+func _on_icon_mouse_entered() -> void:
+	if not global.is_dragging:
+		#draggable = true
+		scale = Vector2(1.05, 1.05)
+
+func _on_icon_mouse_exited() -> void:
+	if not global.is_dragging:
+		#draggable = false
+		scale = Vector2(1.0, 1.00)
+	pass # Replace with function body.
+	
 func _can_drop_data(at_position: Vector2, data: Variant) -> bool:
 	return true
-	
-func _drop_data(at_position: Vector2, data: Variant) -> void:
-	pass
---
+++ scenes/squaretank.gd
@@ -0,0 +1,19 @@
+extends StaticBody2D
+
+
+func _can_drop_data(at_position: Vector2, data: Variant) -> bool:
+	return true
+	
+func _drop_data(at_position: Vector2, data: Variant) -> void:
+	pass
+
+#make a new child to add to scene
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
